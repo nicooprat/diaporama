bf521fcec09e42f8e3aa4e3e99986a8dd2ceeca5
Nico Prat <nicooprat@gmail.com>
2018-12-12 15:34:58 +0100

    Add home footer

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,10 @@
         <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
       </button>
     </form>
+
+    <footer>
+      <a href="https://github.com/nicooprat/zootrope">Fork, credits & readme</a>
+    </footer>
   </main>
 </template>
 
