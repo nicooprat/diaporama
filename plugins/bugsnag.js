8b28e83f8b3b42f463adfa1a6a1ef886ee160ccb
Nico Prat <nicooprat@gmail.com>
2018-12-19 17:33:09 +0100

    Notify Bugsnag only on production

--- plugins/bugsnag.js
+++ plugins/bugsnag.js
@@ -2,5 +2,7 @@ import Vue from 'vue'
 import bugsnag from '@bugsnag/js'
 import bugsnagVue from '@bugsnag/plugin-vue'
 
-const bugsnagClient = bugsnag('c1e2241fdaa9785ed9f9288526498ae4')
-bugsnagClient.use(bugsnagVue, Vue)
+if(process.env.NODE_ENV === 'production') {
+  const bugsnagClient = bugsnag('c1e2241fdaa9785ed9f9288526498ae4')
+  bugsnagClient.use(bugsnagVue, Vue)
+}
